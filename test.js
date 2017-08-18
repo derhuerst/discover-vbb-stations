da982b10a6cfb42a31ae5157a7fb9f00385bb8f9
Jannis R <mail@jannisr.de>
2017-08-18 18:27:19 +0200

    init from discover-db-stations

---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const assert = require('assert')
+
+const walk = require('.')
+
+const assertValidStation = (s) => {
+	assert.strictEqual(s.type, 'station')
+	assert.strictEqual(typeof s.id, 'string')
+	assert.strictEqual(typeof s.name, 'string')
+	assert.ok(s.coordinates)
+	assert.strictEqual(typeof s.coordinates.latitude, 'number')
+	assert.strictEqual(typeof s.coordinates.longitude, 'number')
+}
+
+const data = walk('900000007102') // Berlin Gesundbrunnen
+data.on('error', assert.ifError)
+
+let stations = 0
+data.on('data', (station) => {
+	try {
+		assertValidStation(station)
+	} catch (err) {
+		console.error(err)
+		process.exitCode = 1
+	}
+
+	stations++
+	if (stations >= 100) data.stop()
+})
